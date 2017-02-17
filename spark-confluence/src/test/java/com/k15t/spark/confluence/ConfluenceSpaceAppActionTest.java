81a32714252b51796001cb87937795ab69f36eff
rikuhaa <rikuhaa@iki.fi>
2017-02-17 10:59:41 +0100

    Add more tests / verifications

--- spark-confluence/src/test/java/com/k15t/spark/confluence/ConfluenceSpaceAppActionTest.java
+++ spark-confluence/src/test/java/com/k15t/spark/confluence/ConfluenceSpaceAppActionTest.java
@@ -23,9 +23,14 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
 
@@ -36,6 +41,7 @@ public class ConfluenceSpaceAppActionTest {
     private static ActionConfig actionConfig;
     private static ConfluenceSpaceAppAction actionInstance;
     private static Space spaceMock;
+    private static HttpServletRequest servletRequest;
 
     private static Map<String, Object> velocityContextToReturn = new HashMap<>();
     private static String velocityTemplateToReturn = "mocked template";
@@ -69,7 +75,7 @@ public class ConfluenceSpaceAppActionTest {
         Mockito.when(actionInvocation.getProxy()).thenReturn(actionProxy);
         Mockito.when(actionProxy.getConfig()).thenReturn(actionConfig);
 
-        HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+        servletRequest = mock(HttpServletRequest.class);
 
         Mockito.when(ServletActionContext.getRequest()).thenReturn(servletRequest);
         Mockito.when(servletRequest.getContextPath()).thenReturn(requestContextPath);
@@ -82,15 +88,15 @@ public class ConfluenceSpaceAppActionTest {
         // Set up spies for the
         PowerMockito.mockStatic(DocumentOutputUtil.class);
 
-        Mockito.when(DocumentOutputUtil.generateAdminIframeTemplateContext(Mockito.anyString(), Mockito.anyString(),
-                Mockito.anyString(), Mockito.anyString(), Mockito.anyString())).thenReturn(velocityContextToReturn);
+        Mockito.when(DocumentOutputUtil.generateAdminIframeTemplateContext(anyString(), anyString(),
+                anyString(), anyString(), anyString())).thenReturn(velocityContextToReturn);
 
         Mockito.when(DocumentOutputUtil.getIframeAdminContentWrapperTemplate()).thenReturn(velocityTemplateToReturn);
 
         // Mock up the conflunce velocity util
         PowerMockito.mockStatic(VelocityUtils.class);
 
-        Mockito.when(VelocityUtils.getRenderedContent(Mockito.anyString(), Mockito.anyMap())).
+        Mockito.when(VelocityUtils.getRenderedContent(anyString(), Mockito.anyMap())).
                 thenReturn(renderedVelocityToReturn);
 
         // add a mock for current space context
@@ -101,7 +107,7 @@ public class ConfluenceSpaceAppActionTest {
     }
 
     @Test
-    public void runActionWithIframeAppResourcePath() {
+    public void runActionWithIframeAppResourcePath() throws IOException {
 
         // set the resource path to point to the iframe test app
         actionConfigParams.put("resource-path", "com/k15t/spark/confluence/testiframeapp/");
@@ -115,9 +121,106 @@ public class ConfluenceSpaceAppActionTest {
 
         Assert.assertEquals(Action.INPUT, result);
 
-        String genBody = actionInstance.getBodyAsHtml();
+        // except to be called once and with expected arguments
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.generateAdminIframeTemplateContext(
+                eq(requestContextPath + "/"), eq("spark_space_adm_iframe"),
+                anyString(), isNull(String.class), isNull(String.class));
 
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.getIframeAdminContentWrapperTemplate();
+
+        // check that the admin template and context dummies were used for rendering body
+        // not very ideal test because ties how the rendering is expected to be done which is unimportant...
+        // on the other hand making the VelocityUtils call without mocking it would require lots of other setup
+        PowerMockito.verifyStatic(times(1));
+        VelocityUtils.getRenderedContent(velocityTemplateToReturn, velocityContextToReturn);
+
+        // expect the rendering result to be used as the 'bodyAsHtml' to be inserted into the action's template
+        String genBody = actionInstance.getBodyAsHtml();
         Assert.assertEquals(renderedVelocityToReturn, genBody);
+
+        // the velocity template used is checked DocumentOutputUtil, so here is enough to test that expected content is used
+
+    }
+
+    @Test
+    public void iframeSpaceActionDefaultContextInfo() throws IOException {
+
+        // set the resource path to point to the iframe test app
+        actionConfigParams.put("resource-path", "com/k15t/spark/confluence/testiframeapp/");
+
+        Mockito.when(spaceMock.getKey()).thenReturn("test-space-key");
+
+        String result = actionInstance.index();
+
+        Assert.assertEquals(Action.INPUT, result);
+
+        // except to be called once and with expected arguments
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.generateAdminIframeTemplateContext(
+                anyString(), anyString(),
+                eq("{\"space_key\": \"test-space-key\"}"), isNull(String.class), isNull(String.class));
+
+
+        Mockito.when(spaceMock.getKey()).thenReturn("KEY57");
+
+        result = actionInstance.index();
+
+        Assert.assertEquals(Action.INPUT, result);
+
+        // except to be called once and with expected arguments
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.generateAdminIframeTemplateContext(
+                anyString(), anyString(),
+                eq("{\"space_key\": \"KEY57\"}"), isNull(String.class), isNull(String.class));
+
+
+    }
+
+    @Test
+    public void iframeSpaceActionDefaultQueryParamHandling() throws IOException {
+
+        // set the resource path to point to the iframe test app
+        actionConfigParams.put("resource-path", "com/k15t/spark/confluence/testiframeapp/");
+
+        Mockito.when(servletRequest.getQueryString()).thenReturn("?space_key=TEST&user=admin");
+
+        String result = actionInstance.index();
+
+        Assert.assertEquals(Action.INPUT, result);
+
+        // except to be called once and with expected arguments
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.generateAdminIframeTemplateContext(
+                anyString(), anyString(),
+                anyString(), isNull(String.class), eq("?space_key=TEST&user=admin"));
+
+
+        Mockito.when(servletRequest.getQueryString()).thenReturn("");
+
+        result = actionInstance.index();
+
+        Assert.assertEquals(Action.INPUT, result);
+
+        // except to be called once and with expected arguments
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.generateAdminIframeTemplateContext(
+                anyString(), anyString(),
+                anyString(), isNull(String.class), eq(""));
+
+
+        Mockito.when(servletRequest.getQueryString()).thenReturn("test_param=value&other=42&third=75");
+
+        result = actionInstance.index();
+
+        Assert.assertEquals(Action.INPUT, result);
+
+        // except to be called once and with expected arguments
+        PowerMockito.verifyStatic(times(1));
+        DocumentOutputUtil.generateAdminIframeTemplateContext(
+                anyString(), anyString(),
+                anyString(), isNull(String.class), eq("test_param=value&other=42&third=75"));
 
     }
 
@@ -137,11 +240,17 @@ public class ConfluenceSpaceAppActionTest {
 
         String genBody = actionInstance.getBodyAsHtml();
 
-        Document genBodyDoc = Jsoup.parse(genBody);
+        Document genBodyDoc = Jsoup.parse("<html><head></head><body>" + genBody + "</body></html>");
 
-        Elements theMarkerEr = genBodyDoc.select("#should_be_added");
+        // check that the marker element from the test index-file's body was included in the result
+        Elements theMarkerElement = genBodyDoc.select("#should_be_added");
+        Assert.assertEquals(1, theMarkerElement.size());
 
-        Assert.assertEquals(1, theMarkerEr.size());
+        // test that the scripts and styles with relative paths were fixed to work in Confluence context
+        Assert.assertEquals("/test/app/noniframe/testapp/baseurl/test-script.js",
+                genBodyDoc.body().select("script").get(0).attr("src"));
+        Assert.assertEquals("/test/app/noniframe/testapp/baseurl/test-styles.css",
+                genBodyDoc.body().select("link").get(0).attr("href"));
 
     }
 
