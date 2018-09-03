f06a58fef7af67ec372c657ab908f044614e78b4
Jens Rutschmann <jens@k15t.com>
2018-09-03 15:48:20 +0200

    DEV-54: Replaced usage of OSGi BundleContextAware with Spring's ApplicationContext. Also made DI explicit in spark servlets.
    - Our dependency to the OSGi interface {{BundleContextAware}} required all projects using spark to declare a dependency to a deprecated compatibility library that will go away in future versions of the plugin system. This replaces all code using that interface so it uses spring interfaces instead.
    - Also made the dependency injection of the spark servlets more explicit, so developers creating project-specific subclasses must think about OSGi-importing the respective components.

--- spark-jira/src/main/java/com/k15t/spark/jira/JiraIframeContentServlet.java
+++ spark-jira/src/main/java/com/k15t/spark/jira/JiraIframeContentServlet.java
@@ -1,6 +1,12 @@
 package com.k15t.spark.jira;
 
+import com.atlassian.sal.api.ApplicationProperties;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.message.LocaleResolver;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.templaterenderer.TemplateRenderer;
 import com.k15t.spark.atlassian.AtlassianIframeContentServlet;
+import org.springframework.context.ApplicationContext;
 
 
 /**
@@ -8,5 +14,11 @@ import com.k15t.spark.atlassian.AtlassianIframeContentServlet;
  */
 @Deprecated
 public class JiraIframeContentServlet extends AtlassianIframeContentServlet {
+
+    protected JiraIframeContentServlet(LoginUriProvider loginUriProvider, UserManager userManager, TemplateRenderer templateRenderer,
+            LocaleResolver localeResolver, ApplicationProperties applicationProperties, ApplicationContext applicationContext) {
+
+        super(loginUriProvider, userManager, templateRenderer, localeResolver, applicationProperties, applicationContext);
+    }
 
 }
